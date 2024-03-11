95a8f0c17580336c8f807b7d36dc44de57284c16
Victor <victor.mendele68@gmail.com>
2024-03-11 20:45:15 +0100

    feat: add a binary to run the transport on stdin
    The binary allows to run the transport on the stdin
    like others pino transports.
    
    You can run the transport using
    ```
    npm run start | node dist/bin.js
    ```
    
    where `npm run start` is the process
    that logs in json.

---
+++ src/bin.ts
@@ -0,0 +1,46 @@
+import { version, description } from "../package.json"
+import {program} from "commander";
+import createTransport from "./transport";
+import pump from "pump";
+
+type ProgramOptions = {
+  includeLevelLabel?: boolean
+  levelLabelKey?: string
+  formatTime?: boolean
+  timeKey?: string
+  snakeCase?: boolean
+}
+
+cli()
+
+function cli () {
+  program
+    .version(version)
+    .description(description)
+    .option("--include-level-label", 'add the level as a label', false)
+    .option("--level-label-key", 'the key of the level label')
+    .option("--format-time", "format the timestamp into an ISO date", false)
+    .option("--time-key", "the key that holds the timestamp")
+    .option("--snake-case", 'convert the keys to snake case')
+    .action(async (opts: ProgramOptions) => {
+      const {
+        includeLevelLabel,
+        levelLabelKey,
+        timeKey,
+        formatTime,
+        snakeCase: convertToSnakeCase,
+      } = program.opts<ProgramOptions>()
+
+      const transport = await createTransport({
+        includeLevelLabel,
+        levelLabelKey,
+        timeKey,
+        formatTime,
+        convertToSnakeCase,
+      })
+
+      pump(process.stdin, transport)
+    })
+    .parseAsync(process.argv)
+    .catch(console.error)
+}
