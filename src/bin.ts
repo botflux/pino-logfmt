d4ff92a6971855cac3a09099c389dc2632211fc6
Victor <victor.mendele68@gmail.com>
2024-03-14 20:34:05 +0100

    feat: enable custom levels trough the CLI

--- src/bin.ts
+++ src/bin.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import { version, description } from "../package.json"
 import {program} from "commander";
-import createTransport from "./transport";
+import createTransport, {baseLevelToLabel} from "./transport";
 import pump from "pump";
+import {serialize, parse} from "./levels";
 
 type ProgramOptions = {
   includeLevelLabel?: boolean
@@ -12,6 +13,7 @@ type ProgramOptions = {
   snakeCase?: boolean
   flattenNested?: boolean
   flattenSeparator?: string
+  customLevels?: string
 }
 
 cli()
@@ -21,12 +23,13 @@ function cli () {
     .version(version)
     .description(description)
     .option("--include-level-label", 'add the level as a label', false)
-    .option("--level-label-key", 'the key of the level label', "level_label")
+    .option("--level-label-key <string>", 'the key of the level label', "level_label")
     .option("--format-time", "format the timestamp into an ISO date", false)
-    .option("--time-key", "the key that holds the timestamp", "time")
+    .option("--time-key <string>", "the key that holds the timestamp", "time")
     .option("--snake-case", 'convert the keys to snake case', false)
     .option("--flatten-nested", 'flatten nested metadata', false)
-    .option("--flatten-separator", "the separator used when flattening nested metadata", ".")
+    .option("--flatten-separator <string>", "the separator used when flattening nested metadata", ".")
+    .option("--custom-levels, -x <string>", "the levels associated to their labels in the format \"10:trace,20:debug\"", serialize(baseLevelToLabel))
     .action(async (opts: ProgramOptions) => {
       const {
         includeLevelLabel,
@@ -34,8 +37,13 @@ function cli () {
         timeKey,
         formatTime,
         snakeCase: convertToSnakeCase,
-        flattenNested: flattenNestedObjects
+        flattenNested: flattenNestedObjects,
+        customLevels: serializedCustomLevels
       } = opts
+
+      const customLevels = serializedCustomLevels !== undefined
+        ? parse(serializedCustomLevels)
+        : undefined
 
       const transport = await createTransport({
         includeLevelLabel,
@@ -43,7 +51,8 @@ function cli () {
         timeKey,
         formatTime,
         convertToSnakeCase,
-        flattenNestedObjects
+        flattenNestedObjects,
+        customLevels
       })
 
       pump(process.stdin, transport)
